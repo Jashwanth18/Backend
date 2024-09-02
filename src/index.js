15a6c6a58eafc38680755577d4c64bddc14c7c53
Jashwanth <jashukurma@gmail.com>
2024-09-02 19:43:50 +0530

    added app configs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from "dotenv";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "../db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 connectDB().then(() => {
